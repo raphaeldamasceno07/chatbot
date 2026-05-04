ba0fff763edc6ddb94157cded4a98478faf6ee29
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2026-05-04 12:27:16 -0300

    created signature messageRepository

--- backend/src/repositories/messages-repository.ts
+++ backend/src/repositories/messages-repository.ts
@@ -1,6 +1,6 @@
 import type { IMessage } from '@/models/message-model.js'
 
-export interface IMessageRepository {
+export interface MessageRepository {
   create(data: Partial<IMessage>): Promise<IMessage>
   findByJid(remoteJid: string): Promise<IMessage[]>
 }
